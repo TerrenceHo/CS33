e838b06c5ce88cf65b4d31308985e580f2ce3ecc
Terrence Ho <terrenceho.books@gmail.com>
2017-04-19 23:23:13 -0700

    Finished one problem

---
+++ hw2/loop.c
@@ -0,0 +1,8 @@
+long loop(long x, long n){
+    long result = 0;
+    long mask;
+    for(mask = 1; mask != 0; mask = mask << (0xff & n)){
+        result |= (mask & x);
+    }
+    return result;
+}
---
+++ hw2/switch.c
@@ -0,0 +1,7 @@
+long switch_prob(long x, long n){
+    long result = x;
+    switch(n){
+        /* fill in code here */
+    }
+    return result;
+}
