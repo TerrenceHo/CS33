ae94eef824ffd3b341e66a98670525c007762229
Terrence Ho <terrenceho.books@gmail.com>
2017-04-20 12:46:59 -0700

    Added gbd output

--- hw2/switch.c
+++ hw2/switch.c
@@ -2,6 +2,16 @@ long switch_prob(long x, long n){
     long result = x;
     switch(n){
         /* fill in code here */
+        
+
     }
     return result;
 }
+
+/* gbd output
+ * x/6gx 0x4006f8
+ *
+ * 0x4006f8:  0x00000000004005a1 0x00000000004005c3
+ * 0x400708:  0x00000000004005a1 0x00000000004005aa
+ * 0x400718:  0x00000000004005b2 0x00000000004005bf
+ */
