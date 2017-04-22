b038052202c1cce46bf5afb4cf57ad29af82109d
Terrence Ho <terrenceho.books@gmail.com>
2017-04-21 19:55:04 -0700

    Done with switch.c

--- hw2/switch.c
+++ hw2/switch.c
@@ -2,8 +2,30 @@ long switch_prob(long x, long n){
     long result = x;
     switch(n){
         /* fill in code here */
-        
-
+        case 60:
+            result = x * 8;
+            break;
+        casei 62:
+            result = x * 8;
+            break;
+        case 63:
+            result = x;
+            result = result >> 0x3;
+            break;
+        case 64:
+            result = x;
+            result = result << 0x4;
+            result -= x;
+            x = result;
+            x *= x;
+            result = 75+x;
+            break;
+        case 65:
+            x *= x;
+            result = 75+x;
+            break;
+        default: 
+            result = x + 75
     }
     return result;
 }
