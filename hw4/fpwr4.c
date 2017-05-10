74cb1b85985e26474c6fc92e87abc54f22ee1e3f
Terrence Ho <terrenceho.books@gmail.com>
2017-05-09 21:27:23 -0700

    did some fpwr4

--- hw4/fpwr4.c
+++ hw4/fpwr4.c
@@ -3,4 +3,31 @@
 
 // 2.90
 
+float fpwr2(int x){
+    /* Result exponent and fraction */
+    unsigned exp, frac;
+    unsigned u;
 
+    if(x < -149){
+        /* Too small */
+        exp = ---;
+        frac = ---;
+    } else if (x < ---){
+        /* Denormalized Result */
+        exp = 0;
+        frac = ---;
+    } else if (x < ---){
+        /* Normalized Result */
+        exp = x + 127;
+        frac = 0;
+    } else {
+        /*  Too Big. Return +00*/
+        exp = 255;
+        frac = 0;
+    }
+
+    /* Pack exp and frac into 32 bits */
+    u = exp << 23 | frac;
+    /* Return as float */
+    return utf(u);
+}
